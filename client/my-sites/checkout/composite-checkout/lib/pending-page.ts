6f628e63d04bb5e33aea1ad15ad5629fdda80022
Payton Swick <payton@foolord.com>
2022-07-05 15:59:14 -0400

    Add comments to all new pending page functions

--- client/my-sites/checkout/composite-checkout/lib/pending-page.ts
+++ client/my-sites/checkout/composite-checkout/lib/pending-page.ts
@@ -1,5 +1,8 @@
 import page from 'page';
 
+/**
+ * Redirect to a checkout pending page and from there to a (relative or absolute) url.
+ */
 export function performCheckoutRedirect( url: string, siteSlug: string | undefined ): void {
 	if ( ! isRelativeUrl( url ) ) {
 		return performCheckoutFullPageRedirect( url, siteSlug );
@@ -15,15 +18,24 @@ function isRelativeUrl( url: string ): boolean {
 	return url.startsWith( '/' ) && ! url.startsWith( '//' );
 }
 
+/**
+ * Redirect to a checkout pending page and from there to a relative url.
+ */
 export function performCheckoutSPARedirect( url: string, siteSlug: string | undefined ): void {
 	window.scrollTo( 0, 0 );
 	page( addUrlToPendingPageRedirect( url, siteSlug ) );
 }
 
+/**
+ * Redirect to a checkout pending page and from there to an absolute url.
+ */
 export function performCheckoutFullPageRedirect( url: string, siteSlug: string | undefined ): void {
 	window.location.href = addUrlToPendingPageRedirect( url, siteSlug );
 }
 
+/**
+ * Add a relative or absolute url to the checkout pending page url.
+ */
 export function addUrlToPendingPageRedirect( url: string, siteSlug: string | undefined ): string {
 	const { origin = 'https://wordpress.com' } = typeof window !== 'undefined' ? window.location : {};
 	const successUrlPath = `/checkout/thank-you/${ siteSlug || 'no-site' }/pending`;
