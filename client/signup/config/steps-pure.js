6d34dd912b7526558a1b3b22e09cf7513f82ed54
Panos Kountanis <panosktn@gmail.com>
2022-07-11 19:25:48 +0300

    VideoPress Onboarding Step 2

--- client/signup/config/steps-pure.js
+++ client/signup/config/steps-pure.js
@@ -787,6 +787,12 @@ export function generateSteps( {
 			stepName: 'transfer',
 			dependencies: [ 'siteSlug', 'siteConfirmed' ],
 		},
+
+		'videopress-site': {
+			stepName: 'videopress-site',
+			apiRequestFunction: createWpForTeamsSite,
+			providesDependencies: [ 'siteSlug' ],
+		},
 	};
 }
 
